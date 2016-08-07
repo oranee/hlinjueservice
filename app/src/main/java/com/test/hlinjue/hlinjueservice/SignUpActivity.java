f5cfb1ed0ad6c20c64c05e077e5dd107341a34d1
hlinjue <hlinjue@gmail.com>
2016-08-07 09:52:53 +0700

    SignUp success

--- app/src/main/java/com/test/hlinjue/hlinjueservice/SignUpActivity.java
+++ app/src/main/java/com/test/hlinjue/hlinjueservice/SignUpActivity.java
@@ -5,14 +5,21 @@ import android.content.DialogInterface;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.FormEncodingBuilder;
 import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
 import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -128,6 +135,25 @@ public class SignUpActivity extends AppCompatActivity {
                 .add("Password", passwordString)
                 .add("Avatar", avatarString)
                 .build();
+
+        Request.Builder builder = new Request.Builder();
+        Request request = builder
+                .url("http://swiftcodingthai.com/6aug/add_user_master.php")
+                .post(requestBody)
+                .build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+                Log.d("test123", "e==> " + e.toString());
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                finish();
+            }
+        });
+
 
 
     } // uploadNewValue
